fdfd2abb1813fe7cddea88975932811713843405
satdhruti <satdhruti_paul01@yahoo.com>
2020-03-12 14:17:25 -0800

    first commit

---
+++ app/src/main/java/com/example/devcom_booked/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.devcom_booked;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
+
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    GoogleMap map;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this );
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        map = googleMap;
+
+        LatLng IITB = new LatLng(19.135653, 72.913157);
+        map.addMarker(new MarkerOptions().position(IITB).title("IIT_Bombay"));
+        map.moveCamera(CameraUpdateFactory.newLatLng(IITB));
+    }
+}
